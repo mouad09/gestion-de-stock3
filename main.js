c202f8c04446cc3fec4b151d906f5ca5bf33edc0
mouad09 <mouadessabouni@gmail.com>
2024-03-06 20:03:15 +0100

    corection hofr

---
+++ main.js
@@ -0,0 +1,46 @@
+//  total 
+ 
+ let title = document.getElementsByClassName('title');
+ let price = document.getElementsByClassName('price')[0];
+ let taxes = document.getElementsByClassName('taxes');
+ let ads = document.getElementsByClassName('ads');
+ let discount = document.getElementsByClassName('discount');
+ let total = document.getElementsByClassName('total');
+ let count = document.getElementsByClassName('count');
+ let catogorie = document.getElementsByClassName('catogorie');
+ let submit = document.getElementsByClassName('submit');
+
+console.log(title,price,taxes,ads,discount,total,count,catogorie);
+
+console.log("ddd",price.value)
+// function getTotal(){
+//     if(price.value !== ''){
+//         let result = (parseInt(price.value) + parseInt(taxes.value) + parseInt(ads.value) - parseInt(discount.value)) ;
+//         total.innerHTML = result;
+//         total.style.background = "#040";
+//     }
+//     if (price.value !== '' && !isNaN(parseFloat(price.value)) &&
+//     taxes.value !== '' && !isNaN(parseFloat(taxes.value)) &&
+//     ads.value !== '' && !isNaN(parseFloat(ads.value)) &&
+//     discount.value !== '' && !isNaN(parseFloat(discount.value))) {
+    
+//     let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+
+//     total.innerHTML = result;
+//     total.style.background = "#040";
+//     // console.log(price,taxes,ads,discount,total,count,catogorie);
+// } else {
+//     console.error("Les valeurs ne sont pas valides.");
+// }
+// }
+
+  
+  // create product 
+  // save localstorige
+  // clear input 
+// read 
+// count 
+// delete 
+// update 
+// search 
+// clear data
